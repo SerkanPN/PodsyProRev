10d16650f9429fc218232316de8347ee243ad3bd
Podsy Admin <admin@podsy.pro>
2026-07-02 23:50:40 +0700

    Add real Google Client ID

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 import { AppProvider } from './AppContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '999999999999-dummy.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '43553473187-9dmt2g2raecfu7sbctj0vahc4u17c9g0.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
